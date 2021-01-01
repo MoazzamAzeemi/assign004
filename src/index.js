e31b6b7f31ad58e10e3cf0af90fd87f889d3719d
MoazzamAzeemi <38378900+MoazzamAzeemi@users.noreply.github.com>
2021-01-01 23:51:10 +0500

    updated
    commented import of App component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Room from './room.js';
-import App from './App.js';
+//import App from './App.js';
 
 ReactDOM.render(
   <>
